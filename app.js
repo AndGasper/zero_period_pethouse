adf32ce7ecaceecacfe62038129b5066d6520252
Daniel Paschal <daniel.paschal@learningfuze.com>
2017-04-21 08:20:09 -0700

    initial comit

---
+++ app.js
@@ -0,0 +1,30 @@
+
+//main controller creating pets and governing interactions between pets
+function App(){
+
+}
+
+//controller for cat objects
+function Cat(){
+
+}
+
+//controller for dog objects
+function Dog(){
+
+}
+
+//controller for toy objects that can be played with by animals
+function Toy(){
+
+}
+
+//controller for food dish to supply food to other animals
+function FoodDish(){
+
+}
+
+//what goes in, must come out
+function LitterBox(){
+
+}
