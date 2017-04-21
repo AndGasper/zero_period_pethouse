b642b2fa75ed2dbc449d10b73ded1abef21f5b9e
Kchalmers <kevinqchal@yahoo.com>
2017-04-21 09:30:33 -0700

    app arrays

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 
 //main controller creating pets and governing interactions between pets
 function App(){
-
+let cats = [{name: 'Cat1', age: '3', type: 'cat'}, {name: 'Kitty', age: '20', type: 'cat'}, {name: 'Dog', age: '5', type: 'cat'}];
+let dogs = [{name: 'Fido', age: '3', type: 'dog'}, {name: 'Cat2', age: '20', type: 'dog'}, {name: 'Cat3', age: '5', type: 'dog'}];
+let toys = [{name: 'ball', type: 'ball'}, {name: 'rope', type: 'rope'}, {name: 'toy', type: 'toy'}];
 }
 
 //controller for cat objects
